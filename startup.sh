2a0719d8038532fe3457c007cf6434e8912a0d4d
Dmitry Antonenko <raerayan@gmail.com>
2018-08-20 00:53:50 +0700

    Add yarn.

--- startup.sh
+++ startup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-# docker build -t react-app-img .
-# docker run -d --name react-app-cont -v ./:/project react-app-img
-
+# If the application folder is empty run create-react-app, otherwise just start.
 if [ -z "$(ls -A /project/$@)" ]; then
-    /usr/local/bin/create-react-app $@ && cd /project/$@ && /usr/local/bin/npm start
+    /usr/local/bin/create-react-app $@ && cd /project/$@ && /usr/local/bin/yarn start
 else
     cd /project/$@ && /usr/local/bin/npm start
 fi
