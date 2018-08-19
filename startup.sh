611813d7c4158848c3525eb3ab097e3a0c978c85
Dmitry Antonenko <raerayan@gmail.com>
2018-08-20 00:37:13 +0700

    Fix 5.

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # docker build -t react-app-img .
 # docker run -d --name react-app-cont -v ./:/project react-app-img
-/usr/local/bin/create-react-app $@ && cd /project/$@ && /usr/local/bin/npm start
+
+if [ -z "$(ls -A /project/$@)" ]; then
+    /usr/local/bin/create-react-app $@ && cd /project/$@ && /usr/local/bin/npm start
+else
+    cd /project/$@ && /usr/local/bin/npm start
+fi
